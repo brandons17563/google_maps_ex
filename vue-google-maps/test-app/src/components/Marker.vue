af561fd82d5a8c0b1b3caaed9a64a4558493c993
Chris Pecoraro <cpecoraro18@gmail.com>
2021-04-27 11:00:01 -0500

    added chris part

---
+++ vue-google-maps/test-app/src/components/Marker.vue
@@ -0,0 +1,33 @@
+<script>
+  export default {
+    name: "MapMarker",
+    props: {
+      lat: {
+        type: Number,
+        required: true
+      },
+      lng: {
+        type: Number,
+        required: true
+      }
+    },
+    data: () => ({
+      marker: null
+    }),
+    mounted() {
+      this.$parent.getMap(map => {
+        this.marker = new window.google.maps.Marker({
+          position: {lat: this.lat, lng: this.lng },
+          map: map
+        })
+      })
+    },
+    beforeDestroy() {
+      this.marker.setMap(null)
+      window.google.maps.events.clearInstanceListeners(this.marker);
+    },
+    render() {
+      return null
+    }
+  }
+</script>
