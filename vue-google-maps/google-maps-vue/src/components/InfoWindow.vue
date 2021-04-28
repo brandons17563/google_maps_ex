aee686ee22a3cf901eeedcb7d502ff35ce789002
Chris Pecoraro <cpecoraro18@gmail.com>
2021-04-28 10:47:14 -0500

    added base infowindow component

---
+++ vue-google-maps/google-maps-vue/src/components/InfoWindow.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "InfoWindow",
+    props: {
+      lat: {
+        type: Number,
+        required: true
+      },
+      lng: {
+        type: Number
+        required: true
+      }
+    },
+  }
+</script>
